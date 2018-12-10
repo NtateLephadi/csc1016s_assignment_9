3f4ff1b066899e726672c6c40c7c7e6a9747e2db
Tumelo Lephadi <lphtum003@myuct.ac.za>
2018-12-10 05:05:44 +0200

    completely done

--- Question3.java
+++ Question3.java
@@ -7,20 +7,56 @@ public class Question3{
       String line = scanner.nextLine();
       char[] tokens = line.toCharArray();
       for (char token: tokens){
-         if (Character.isDigit(token))
+         if (Character.isDigit(token)){
             link.add(token);
+          }
+          else if(!Character.isDigit(token) || !(token=='-') || !(token=='*') || !(token=='+') || !(token=='\\')){
+            System.out.println("Integer expected but not found");
+            System.exit(0);
+          }
          else if(token == '-'){
-            link.push(Integer.valueOf(link.pop().toString()) - Integer.valueOf(link.pop().toString())); 
+            if (link.size() < 2){
+              System.out.println("Insufficient arguments for -");
+              System.exit(0);
+            }
+            else{
+              link.push( - Integer.valueOf(link.pollLast().toString()) + Integer.valueOf(link.pollLast().toString()));
+            }
          }
          else if(token == '+'){
-            link.push(Integer.valueOf(link.pop().toString()) + Integer.valueOf(link.pop().toString())); 
+           if (link.size() < 2){
+             System.out.println("Insufficient arguments for +");
+             System.exit(0);
+           }
+           else{
+             link.push(Integer.valueOf(link.pollLast().toString()) + Integer.valueOf(link.pollLast().toString()));
+            }
          }
          else if(token == '*'){
-            link.push(Integer.valueOf(link.pop().toString()) * Integer.valueOf(link.pop().toString())); 
+           if (link.size() < 2){
+             System.out.println("Insufficient arguments for *");
+             System.exit(0);
+           }
+           else{
+            link.push(Integer.valueOf(link.pollLast().toString()) * Integer.valueOf(link.pollLast().toString()));
+          }
          }
          else if(token == '/'){
-            link.push(Integer.valueOf(link.pop().toString()) / Integer.valueOf(link.pop().toString())); 
+           if (link.size() < 2){
+             System.out.println("Insufficient arguments for /");
+             System.exit(0);
+           }
+           else{
+            link.push(Integer.valueOf(link.pollLast().toString()) / Integer.valueOf(link.pollLast().toString()));
+          }
          }
+      }
+      if (link.size() > 1){
+        System.out.println("Extra symbols in expression");
+        System.exit(0);
+      }
+      else{
+        System.out.println("Answer: " + link.pop());
       }
    }
 }
